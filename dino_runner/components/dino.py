03183ef8ea02ee472379be304388cce77d780a50
N-at-a-l-y <nvglombana@gmail.com>
2023-05-01 23:40:18 -0500

    clase_3

--- dino_runner/components/dino.py
+++ dino_runner/components/dino.py
@@ -12,7 +12,7 @@ class Dino:
 		self.image = RUNNING[0]
 		self.dino_rect = self.image.get_rect()					
 		self.dino_rect.x = self.X_POS						
-		self.dino_rect.y = self.Y_POS	
+		self.dino_rect.y = self.Y_POS
 		self.dino_run = True 
 		self.step_index = 0
 		self.dino_jump = False
@@ -37,8 +37,7 @@ class Dino:
 		if user_imput[pygame.K_UP] and not self.dino_jump:
 			self.dino_jump = True
 			self.dino_run = False			
-		elif not self.dino_jump:
-			self.dino_run = True
+
 		if user_imput[pygame.K_DOWN] and not self.dino_jump:
 			self.dino_duck = True
 			self.dino_run = False
@@ -55,9 +54,11 @@ class Dino:
 
 	def jump(self):
 		self.image = JUMPING
+        # En el eje y
 		self.dino_rect.y -= self.jump_speed*4
 		self.jump_speed -= 0.8
-		if self.dino_rect.y > 300:
+		if self.jump_speed < -self.JUMP_SPEED:
+			self.dino_rect.y = self.Y_POS
 			self.dino_jump = False
 			self.jump_speed = self.JUMP_SPEED
 	  
@@ -69,5 +70,6 @@ class Dino:
 		self.dino_rect.y = self.Y_POS_DUCK
 		self.step_index += 1
 		if not user_imput[pygame.K_DOWN]:
-			self.dino_duck = False
+			self.dino_duck = False #poner en el update
 			self.dino_run = True
+			
